eb30921c016896cefc93114cbb70d19c66c296c8
kevinsmithseven <kevinsmithseven@gmail.com>
2023-10-22 13:17:55 -0500

    updated database.js to include Database class constructor for db queries, will continue to add to this class

--- database.js
+++ database.js
@@ -3,11 +3,36 @@ const mysql = require('mysql2/promise');
 class Database {
     constructor(config) {
         this.config = config;
-    }
+    };
 
     async createConnection() {
         return await mysql.createConnection(this.config);
-    }
+    };
 
-    async viewAllDepartments
+    async viewAllDepartments(connect) {
+        try {
+            const [results] = await connect.query('SELECT * FROM departments');
+            return results;
+        }   catch (error) {
+            throw new Error('Failed to retrieve departments');
+        };
+    };
+
+    async viewAllRoles(connect) {
+        try {
+            const [results] = await connect.query('SELECT * FROM roles');
+            return results;
+        }   catch (error) {
+            throw new Error('Failed to retrieve roles');
+        };
+    };
+
+    async viewAllEmployees(connect) {
+        try {
+            const [results] = await connect.query('SELECT * FROM employees');
+            return results;
+        }   catch (error) {
+            throw new Error('Failed to retrieve employees');
+        };
+    };
 }
