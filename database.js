d1792c971ea1f9445b24d6e4ff65e411c051de07
kevinsmithseven <kevinsmithseven@gmail.com>
2023-10-22 15:18:36 -0500

    added export to database.js module, created helpers.js, intended to contain functionality for db queries, added initial db connection code and inquirer prompts in index.js

--- database.js
+++ database.js
@@ -35,4 +35,7 @@ class Database {
             throw new Error('Failed to retrieve employees');
         };
     };
-}
+};
+
+
+module.exports = Database;
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const mysql = require('mysql2/promise');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 const Database = require('./database.js');
@@ -12,6 +11,56 @@ const db = new Database(
         password: 'rootroot',
         database: 'employee_db'
     },
-    console.log(`Connected to the employee_db database.`)
 );
+
+
+async function userPrompt() {
+    const connect = await db.createConnection();
+    console.log(`Connected to the employee_db database.`)
+
+    try {
+        const userRequest = await inquirer.prompt([
+            {
+                type: 'list',
+                name: 'action',
+                message: 'What would you like to do?',
+                choices: [
+                    'View all departments',
+                    'View all roles',
+                    'View all employees',
+                    'View employees by manager',
+                    'Add a department',
+                    'Add a role',
+                    'Add an employee',
+                    'Update an employee role',
+                    'Delete a department',
+                    'Delete a role',
+                    'Delete an employee',
+                    'Exit',
+                ]
+            }
+        ]);
+
+        switch (userRequest.action) {
+            case 'View all departments':
+                await viewAllDepartments(connect);
+                break;
+            case 'Exit':
+                await connect.end();
+                break;
+            // ... handle other cases
+        }
+    } catch (error) {
+        console.error('Error:', error.message);
+        await connect.end();
+    };
+};
+
+userPrompt();
+
+
+
+
+
+
 
