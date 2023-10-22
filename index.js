12eb96b9ad878585e8e8b5d26f4056964736bffc
kevinsmithseven <kevinsmithseven@gmail.com>
2023-10-22 13:37:23 -0500

    instantiated new Database class in index.js with console log

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const mysql = require('mysql2/promise');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+const Database = require('./database.js');
+
+const db = new Database(
+    {
+        host: 'localhost',
+        // MySQL username
+        user: 'root',
+        // MySQL password
+        password: 'rootroot',
+        database: 'employee_db'
+    },
+    console.log(`Connected to the employee_db database.`)
+);
 
