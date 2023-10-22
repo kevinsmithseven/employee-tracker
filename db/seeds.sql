a45617682085ac4a761e6da6e1bebd4cbc6bebd0
kevinsmithseven <kevinsmithseven@gmail.com>
2023-10-22 10:44:17 -0500

    changed seeds.sql initial values for roles table

--- db/seeds.sql
+++ db/seeds.sql
@@ -13,16 +13,16 @@ INSERT INTO departments (dept_name) VALUES
 
 -- Insert initial roles
 INSERT INTO roles (title, salary, department_id) VALUES
-    ('Captain', 120000.00, 1),
-    ('Chief Engineer', 95000.00, 2),
-    ('Science Officer', 90000.00, 3),
-    ('Intelligence Officer', 93000.00, 4),
-    ('Diplomatic Envoy', 91000.00, 5),
-    ('Chief Medical Officer', 95000.00, 6),
-    ('Communications Officer', 88000.00, 7),
-    ('Tactical Officer', 92000.00, 8),
-    ('Helmsman', 89000.00, 2),  -- Helmsman is also part of Operations
-    ('Academy Instructor', 87000.00, 10);
+    ('Astro-Logistics Officer', 92000.00, 1),
+    ('Quantum Programmer', 105000.00, 2),
+    ('Cyber-Human Mediator', 88000.00, 3),
+    ('Galactic Ambassador', 120000.00, 4),
+    ('Terraform Engineer', 95000.00, 5),
+    ('Telepathy Specialist', 86000.00, 6),
+    ('Time-Travel Coordinator', 112000.00, 7),
+    ('Bio-Enhancement Scientist', 98000.00, 8),
+    ('Virtual World Architect', 90000.00, 9),
+    ('Zero-Energy Innovator', 115000.00, 10);
 
 -- Insert initial employees
 INSERT INTO employees (first_name, last_name, manager_id, role_id) VALUES
